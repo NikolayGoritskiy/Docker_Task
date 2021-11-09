c072570e66cd238fa55a1edcdbb5e08556ad2902
NikolayGoritskiy <terry2600@mail.ru>
2021-11-10 02:04:58 +0500

    edit PubApi

--- frontend/src/PubApi.js
+++ frontend/src/PubApi.js
@@ -1,52 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://localhost:8000/server' {
-
-  listen 80;
-
-  location /nginx_status {
-    stub_status on;
-    access_log off;
-    allow 127.0.0.1;
-    allow 172.16.0.0/12;
-    deny all;
-  }
-
-  location / {
-    root   /usr/share/nginx/html;
-    index  index.html index.htm;
-    try_files $uri $uri/ /index.html;
-  }
-
-  location /api {
-    proxy_pass http://server:9013;
-    }
-
-  location /api/payments {
-    proxy_pass http://p-service:4004;
-    }
-
-  location /sitemap.xml {
-    proxy_pass http://server:9013/sitemap.xml;
-    }
-
-  location /socket.io/ {
-#    proxy_pass              http://upstream-nodejs;
-    proxy_pass http://server:9013;
-
-    proxy_http_version      1.1;
-    proxy_set_header        Upgrade                 $http_upgrade;
-    proxy_set_header        Connection              "upgrade";
-    proxy_set_header        Host                    $host;
-    proxy_set_header        X-Real-IP               $remote_addr;
-    proxy_set_header        X-Forwarded-For         $proxy_add_x_forwarded_for;
-    }
-
-  error_page   500 502 503 504  /50x.html;
-
-  location = /50x.html {
-    root   /usr/share/nginx/html;
-  }
-api/v1/lib/publishing_house/';
+const API_URL = 'http://localhost:8000/api/v1/lib/publishing_house/';
 
 export default class PubManager {
 
