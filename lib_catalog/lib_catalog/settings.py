a7cd2767bffd9daa5cd55e7241518354daccfdfc
NikolayGoritskiy <terry2600@mail.ru>
2021-11-10 03:35:43 +0500

    edit dockerfile and settings.py

--- lib_catalog/lib_catalog/settings.py
+++ lib_catalog/lib_catalog/settings.py
@@ -135,6 +135,8 @@ REST_FRAMEWORK = {
                             'rest_framework.filters.SearchFilter']
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
 ]
